48311cf207b7eac84efa5f596c300c736d51048f
ashuklaaa <aarushishukla02@gmail.com>
2026-08-18 23:34:53 +0530

    Add files via upload

---
+++ Student Marks.py
@@ -0,0 +1,9 @@
+m1=float(input("Enter marks of subject 1 :"))
+m2=float(input("Enter marks of subject 2 :"))
+m3=float(input("Enter marks of subject 3 :"))
+m4=float(input("Enter marks of subject 4 :"))
+m5=float(input("Enter marks of subject 5 :"))
+s=m1+m2+m3+m4+m5
+print("Total marks :",s)
+print("Average marks :",s/5)
+print("Percentage :",(s/500)*100)
