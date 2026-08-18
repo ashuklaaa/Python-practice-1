9da9fcb368caaf8800866086c1c962771e7fb979
ashuklaaa <aarushishukla02@gmail.com>
2026-08-18 23:34:16 +0530

    Add files via upload

---
+++ Student Info.py
@@ -0,0 +1,8 @@
+n=input("Enter your name : ")
+a=int(input("Enter your age : "))
+c=input("Enter college name : ")
+b=input("Enter branch name : ")
+print("Name:",n,"\n"
+      "Age:",a,"\n"
+      "College Name:",c,"\n"
+      "Branch:",b)
